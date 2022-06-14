35ed0ee87a6d19669e09f31573fa45c83f6e8f0d
rocio <rocio.gabriela.03c@gmail.com>
2022-06-14 22:07:42 +0200

    contacto vendedor

--- front/src/pages/shop/ContactSeller.vue
+++ front/src/pages/shop/ContactSeller.vue
@@ -2,8 +2,13 @@
 <h1>Carrito</h1>
 <div v-for="product in selected_products" :key="product.id">
   <h2>{{product.product_name}}</h2>
-  <img :src="product.product_img" alt="">
+  <img :src="product.product_img" alt=""/>
   <button @click="removeProducts(product)" class="remove_button" >Eliminar producto</button>
+
+<div> {{contact_seller.seller_email}}</div>
+  <div> {{contact_seller.seller_id}}</div>
+  <img :src="contact_seller.seller_logo" alt="Sample photo">
+
 </div>
 </template>
 
@@ -11,19 +16,31 @@
 export default {
     data() {
         return {
-        selected_products: []
+        selected_products: [],
+        contact_seller: []
         }
     },
     mounted() {
         this.loadData()
+        this.loadSeller()
     },
 
     methods: {
+      async loadSeller(){
+        let seller_id = await this.loadData()[0].seller_id
+        let response = await fetch("http://localhost:5000/api/sellers/" + seller_id)
+        let data = await response.json()
+        this.contact_seller = data
+        return data
+      },
+
       loadData() {
         let products = localStorage.getItem("selectedProducts")
         let jsonProducts = JSON.parse(products)
         this.selected_products = jsonProducts
+        return jsonProducts
     },
+    
       removeProducts(product) {
         let index = this.selected_products.indexOf(product)
         this.selected_products.splice(index, 1)
@@ -35,6 +52,13 @@ export default {
 <style>
 img {
   width: 9rem;
+}
+
+
+div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 </style>
